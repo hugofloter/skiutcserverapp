28825c9bad53f7e23d092aafb64915434ef742f2
quentin <user.name>
2019-12-19 16:17:21 +0100

    add model and view user

---
+++ user/model.py
@@ -0,0 +1,15 @@
+class User():
+    def __init__(self, data):
+        self.login = data[0]
+        self.lastname = data[1]
+        self.firstname = data[2]
+        self.email = data[3]
+        self.password = data[4]
+
+    def to_json(self):
+        return {
+            'login': self.login,
+            'lastname': self.lastname,
+            'firstname': self.firstname,
+            'email': self.email,
+        }
