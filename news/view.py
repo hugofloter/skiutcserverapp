9f3d1dc87e5d38d911f9b1ef2104c0e9dff7c106
quentin <user.name>
2019-12-19 17:21:21 +0100

    all routes for news

--- news/view.py
+++ news/view.py
@@ -1,6 +1,7 @@
 from news.model import News
 from db import dbskiutc_con as db
 from utils.errors import Error
+from datetime import datetime
 
 class NewsView():
     def __init__(self):
@@ -21,6 +22,60 @@ class NewsView():
                     result[count] = value.to_json()
                     count +=1;
                 return result
-                
+
         except Exception as e:
+            return e
+
+    def get(self, id):
+        try:
+            with self.con:
+                cur = self.con.cursor(Model = News)
+                sql = "SELECT * from news WHERE id = %s";
+                cur.execute(sql, id)
+
+                response = cur.fetchone()
+
+                if response is None:
+                    return {}
+
+                return response.to_json()
+
+        except Exception as e:
+            return e
+
+    def create(self, data):
+        try:
+            with self.con:
+                cur = self.con.cursor(Model = News)
+                sql = "INSERT INTO news (title, text, photo, date, type) VALUES (%s, %s, %s, %s, %s);"
+
+                now = datetime.now()
+                now = now.strftime('%Y-%m-%d %H:%M:%S')
+                cur.execute(sql, (data['title'], data['text'], data['photo'], now, data['type']))
+
+                self.con.commit()
+
+                sql =  "SELECT * FROM news ORDER BY ID DESC;"
+                cur.execute(sql)
+                last = cur.fetchone()
+
+                return last.to_json()
+
+        except Exception as e:
+            self.con.rollback()
+            return e
+
+    def delete(self, id):
+        try:
+            with self.con:
+                cur = self.con.cursor(Model = News)
+                sql = "DELETE FROM news WHERE id = %s;";
+                cur.execute(sql, (id))
+
+                self.con.commit()
+
+                return self.list()
+
+        except Exception as e:
+            self.con.rollback()
             return e
--- webapis/news.py
+++ webapis/news.py
@@ -1,14 +1,46 @@
+import json
+
 from bottle import request, response
-from bottle import get, post, put
+from bottle import get, post, delete
 from news.view import NewsView
-from utils.middlewares import authenticate
+from utils.middlewares import authenticate, admin
+from utils.errors import Error
 
 @get('/v1/news')
+@authenticate
 def get_news(user=None):
     """get all news"""
     try:
-        view = NewsView()
-
-        return view.list()
+        return NewsView().list()
     except Exception as e:
         return e;
+
+@get('/v1/news/<id>')
+@authenticate
+def get_one_news(id, user=None):
+    """get only one new"""
+    try:
+        return NewsView().get(id)
+    except Exception as e:
+        return e
+
+
+@post('/v1/news')
+@admin
+def create_news(user = None):
+    """create a news"""
+    try:
+        data = json.loads(request.body.read())
+
+        return NewsView().create(data)
+    except Exception as e:
+        return e
+
+@delete('/v1/news/<id>')
+@admin
+def delete_news(id, user=None):
+    """delete a news"""
+    try:
+        return NewsView().delete(id)
+    except Exception as e:
+        return e
