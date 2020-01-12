ca7bbb7c6e516294328206d43c50f6d01b66f96d
hugofloter <hugo.paigneau@etu.utc.fr>
2020-01-12 15:25:16 +0100

    small fix

--- webapis/animation.py
+++ webapis/animation.py
@@ -7,14 +7,22 @@ from utils.middlewares import authenticate, admin
 @get('/animation')
 @authenticate
 def get_user_level(user=None):
-    """get all news"""
+    """get user level"""
     try:
         login = user.to_json().get('login')
-        is_admin = user.to_json().get('isAdmin')
-        if is_admin:
-            return AnimationView(login=login, admin=is_admin).get_top_users()
-        else:
-            return AnimationView(login=login).get_user_level()
+        return AnimationView(login=login).get_user_level()
+
+    except Exception as e:
+        return e
+
+
+@get('/animation/admin')
+@admin
+def get_user_level(user=None):
+    """get top user level"""
+    try:
+        login = user.to_json().get('login')
+        return AnimationView(login=login, admin=True).get_top_users()
 
     except Exception as e:
         return e
@@ -23,7 +31,7 @@ def get_user_level(user=None):
 @get('/animation/<key>')
 @authenticate
 def update_animation(key, user=None):
-    """get all news"""
+    """unlock level"""
     try:
         login = user.to_json().get('login')
         return AnimationView(login=login).unlock_level(key, user)
