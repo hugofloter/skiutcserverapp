12be44334ffd4dc6b1fc0522fc13d264db00bf83
hugofloter <hugo.paigneau@etu.utc.fr>
2020-01-26 20:38:35 +0100

    updatge

--- bot/view.py
+++ bot/view.py
@@ -160,7 +160,7 @@ class BotView():
             return self.send_question(sender_psid)
 
         if message and len(message) and ("stats" in message.lower() or "statistiques" in message.lower()):
-            return self.send_question(sender_psid)
+            return self.send_stats(sender_psid)
 
         if message:
             return self.basic_answer(sender_psid)
@@ -390,16 +390,7 @@ class BotView():
             })
 
         response = {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "generic",
-                    "elements": [{
-                        "title": "Récupère les statistique pour une question !",
-                        "buttons": buttons
-                    }]
-                }
-            }
+            "text": "PIOIOOO"
         }
 
         self.callSendAPI(sender_psid, response)
