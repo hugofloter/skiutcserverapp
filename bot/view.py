8febc73ec26429a3edffba44c58518e5c1208d38
hugofloter <hugo.paigneau@etu.utc.fr>
2020-01-26 18:28:32 +0100

    add stats

--- bot/view.py
+++ bot/view.py
@@ -160,7 +160,7 @@ class BotView():
             return self.send_question(sender_psid)
 
         if message and len(message) and ("stats" in message.lower() or "statistiques" in message.lower()):
-            return self.send_question(sender_psid)
+            return self.send_stats(sender_psid)
 
         if message:
             return self.basic_answer(sender_psid)
